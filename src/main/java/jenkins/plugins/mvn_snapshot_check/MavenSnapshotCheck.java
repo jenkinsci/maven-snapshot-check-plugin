341c50ea20eab3cf2034d6956a67f286680346d4
donghui <977675308@qq.com>
2022-10-02 12:39:34 +0800

    support building against jdk 11 (#33)
    * support jdk 11
    
    * build with jdk 11
    
    * Use <code> tag rather than <tt> as <tt> is deprecated
    
    * remove javaLevel parameter

--- src/main/java/jenkins/plugins/mvn_snapshot_check/MavenSnapshotCheck.java
+++ src/main/java/jenkins/plugins/mvn_snapshot_check/MavenSnapshotCheck.java
@@ -18,7 +18,7 @@ import org.apache.tools.ant.types.FileSet;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
-import javax.annotation.Nonnull;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.regex.Matcher;
@@ -113,7 +113,7 @@ public class MavenSnapshotCheck extends Builder implements SimpleBuildStep{
      * @param taskListener
      */
     @Override
-    public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener taskListener) {
+    public void perform(@NonNull Run<?, ?> run, @NonNull FilePath workspace, @NonNull Launcher launcher, @NonNull TaskListener taskListener) {
         if (getCheck()) {
             run.addAction(new MavenSnapshotCheckAction(CHECKED));
             String message = "[Maven SNAPSHOT Check], pomFiles: " + getPomFiles();
@@ -145,7 +145,7 @@ public class MavenSnapshotCheck extends Builder implements SimpleBuildStep{
      * The class is marked as public so that it can be accessed from views.
      *
      * <p>
-     * See <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+     * See <code>src/main/resources/jenkins/plugins/mvn_snapshot_check/MavenSnapshotCheck/config.jelly</code>
      * for the actual HTML fragment for the configuration screen.
      */
     @Symbol("mavenSnapshotCheck")
